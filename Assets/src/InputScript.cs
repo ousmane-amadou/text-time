9e87f4caefd7f2489b10914e9b3c09baaadea9fe
ousmane-amadou <ousmane.amadou@mail.utoronto.ca>
2018-01-20 14:15:37 -0500

    expirmenting with touch keyboard

--- Assets/src/InputScript.cs
+++ Assets/src/InputScript.cs
@@ -6,21 +6,11 @@ public class InputScript : MonoBehaviour {
     public string stringToEdit = "Hello World";
     private TouchScreenKeyboard keyboard;
 
+    // Opens native keyboard
     void OnGUI()
-    {   
+    {
         stringToEdit = GUI.TextField(new Rect(10, 10, 200, 30), stringToEdit, 30);
-        keyboard = TouchScreenKeyboard.Open(stringToEdit, TouchScreenKeyboardType.Default);
-        //foreach (Touch touch in Input.touches)
-        //{
-        //    string message = "";
-        //    message += "ID: " + touch.fingerId + "\n";
-        //    message += "Phase: " + touch.phase.ToString() + "\n";
-        //    message += "TapCount: " + touch.tapCount + "\n";
-        //    message += "Pos X: " + touch.position.x + "\n";
-        //    message += "Pos Y: " + touch.position.y + "\n";
-
-        //    int num = touch.fingerId;
-        //    GUI.Label(new Rect(0 + 130*num, 0, 120, 100), message);
-        //}    
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
+        Debug.Log(TouchScreenKeyboard.visible);
     }
 }
