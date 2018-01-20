f6ecdc63867ec7325f9dfa19b3de3bb9b1b4ff8e
ousmane-amadou <ousmane.amadou@mail.utoronto.ca>
2018-01-20 14:00:44 -0500

    trying to addedmobile keyboard

--- Assets/src/InputScript.cs
+++ Assets/src/InputScript.cs
@@ -3,26 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InputScript : MonoBehaviour {
+    public string stringToEdit = "Hello World";
+    private TouchScreenKeyboard keyboard;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-
-    // Update is called once per frame
     void OnGUI()
-    {
-        foreach (Touch touch in Input.touches)
-        {
-            string message = "";
-            message += "ID: " + touch.fingerId + "\n";
-            message += "Phase: " + touch.phase.ToString() + "\n";
-            message += "TapCount: " + touch.tapCount + "\n";
-            message += "Pos X: " + touch.position.x + "\n";
-            message += "Pos Y: " + touch.position.y + "\n";
+    {   
+        stringToEdit = GUI.TextField(new Rect(10, 10, 200, 30), stringToEdit, 30);
+        keyboard = TouchScreenKeyboard.Open(stringToEdit, TouchScreenKeyboardType.Default);
+        //foreach (Touch touch in Input.touches)
+        //{
+        //    string message = "";
+        //    message += "ID: " + touch.fingerId + "\n";
+        //    message += "Phase: " + touch.phase.ToString() + "\n";
+        //    message += "TapCount: " + touch.tapCount + "\n";
+        //    message += "Pos X: " + touch.position.x + "\n";
+        //    message += "Pos Y: " + touch.position.y + "\n";
 
-            int num = touch.fingerId;
-            GUI.Label(new Rect(0 + 130*num, 0, 120, 100), message);
-        }    
+        //    int num = touch.fingerId;
+        //    GUI.Label(new Rect(0 + 130*num, 0, 120, 100), message);
+        //}    
     }
 }
