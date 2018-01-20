7d7774534b312711da98f94a3c60bfc869df8fee
Ge0rges <ge0rges@ge0rges.com>
2018-01-20 13:55:44 -0500

    Added timer class and Object

--- Assets/src/Game.cs
+++ Assets/src/Game.cs
@@ -1,51 +1,52 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
-//public class NewBehaviourScript : MonoBehaviour {
-//    public Dictionary charComplexity; 
-//    // Maps a character to how hard it would be to type on a mobile 
+public class NewBehaviourScript : MonoBehaviour {
+//   public Dictionary charComplexity; 
+//   // Maps a character to how hard it would be to type on a mobile 
 //    // screen
     
 //    public int score;
 //    public int combo;
 //    public int accuracy;
 //    public int multiplierValue;
-    
+//    
 //    public bool isPaused;
-
+//
 //    public List<string> gameWords;
 //    public List<TextBlob> gameBlobs;
-//	// Use this for initialization
-//	void Start () {
-		
-//	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
     
-//	// Update is called once per frame
-//    void Update () {
-    
-//    }
+	// Update is called once per frame
+    void Update () {
+
+    }
 	
 //    // Takes in mobileinput
-//	void onSpace () {
+//	void onSpace() {
 //        string playerInput = " ";
 //        for (int i = 0; i < 4; i++) {
 //             if(playerInput == gameBlobs[i].getText()) {
 //                updateScore(gameBlobs[i]);
 //                updateStats(gameBlobs[i]);
-
+//
 //                updateBlobs(i); // Should Generate a New Blob in place of completed blob
 //                boostMultipliers();
 //                return;
 //             }
 //        }
-
+//
 //        // If loop does not terminate, player has not typed in a valid input
 //        // In this case reset multipliers, and deduct points
 //        multiplierValue = 1;
-        
 //	}
-
+//
 //    // Adds points to current score based on:
 //    //   * Length of text completed
 //    //   * Whether or not is a special text blob
@@ -58,11 +59,11 @@
 //            score += finishedTextBlob.bonus; 
 //        }
 //    }
-
+//
 //    // A word complexity score represents how difficult it is to type a
 //    // certain word on a touch screen.
 //    void calculateWordComplexity() { };
-    
+//    
 //    void boostMultipliers() {
 //        if(multiplierValue == 16) {
 //            return;
@@ -70,6 +71,6 @@
 //            multiplierValue *= 2;
 //        } 
 //    }
-    
-    
-//}
+//    
+//    
+}
---
+++ Assets/src/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+	public float timeLeft = 60.0f;// Default is 1 minute.
+	public Text timer;
+
+	// Use this for initialization
+	void Start () {
+		// Update Timer UI
+		timer = gameObject.GetComponent<Text>();
+		timer.text = timeLeft.ToString();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Update time left
+		this.timeLeft -= Time.deltaTime;
+
+		// Update Timer UI
+		timer.text = timeLeft.ToString().Substring(0, 4);
+
+		// End game
+		if (this.timeLeft <= 0) {
+			// Update UI
+		}
+	}
+}
